bbac932af051555842df434738a06e206ed9db9a
Seob <seob_@kakao.com>
2020-07-11 18:42:54 +0900

    first algorithms to commit and test

---
+++ 가운데글자가져오기.py
@@ -0,0 +1,21 @@
+"""
+문제 설명
+
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+재한사항
+s는 길이가 1 이상, 100이하인 스트링입니다.
+"""
+
+
+def solution(s):
+    answer = ''
+    div = int(len(s)/2)
+    if len(s) % 2 == 0:
+        answer = s[div-1:div+1]
+    else:
+        answer = s[div:div+1]
+    return answer
+
+
+# test
+print(solution('powe'))  # 'ow'
